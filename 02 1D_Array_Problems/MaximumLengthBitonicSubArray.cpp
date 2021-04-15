b866c5ffaecb1a0dd4d69f6154f558ca42081545
Soumyadeep Dey <74682979+SoumyadeepDey2002@users.noreply.github.com>
2021-04-15 19:16:30 +0530

    Add files via upload

---
+++ 02 1D_Array_Problems/MaximumLengthBitonicSubArray.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a[1000000];
+
+
+int bitonic(int arr[], int n)
+{
+    int inc[n]; // Length of increasing subarray ending at all indexes
+    int dec[n]; // Length of decreasing subarray starting at all indexes
+    int i, max;
+
+    // length of increasing sequence ending at first index is 1
+    inc[0] = 1;
+
+    // length of increasing sequence starting at first index is 1
+    dec[n-1] = 1;
+
+    // Step 1) Construct increasing sequence array
+    for (i = 1; i < n; i++)
+    inc[i] = (arr[i] >= arr[i-1])? inc[i-1] + 1: 1;
+
+    // Step 2) Construct decreasing sequence array
+    for (i = n-2; i >= 0; i--)
+    dec[i] = (arr[i] >= arr[i+1])? dec[i+1] + 1: 1;
+
+    // Step 3) Find the length of maximum length bitonic sequence
+    max = inc[0] + dec[0] - 1;
+    for (i = 1; i < n; i++)
+        if (inc[i] + dec[i] - 1 > max)
+            max = inc[i] + dec[i] - 1;
+
+    return max;
+}
+
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        cout<<bitonic(a,n)<<endl;
+    }
+
+    return 0;
+}
