fa0b7000daa8300ad6b2e952569f3041d2dcc72a
Soumyadeep Dey <74682979+SoumyadeepDey2002@users.noreply.github.com>
2021-04-26 22:34:28 +0530

    Add files via upload

---
+++ 08 SubMatrix_Problems/SumOfAllSubmatrices_Bruteforce.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a[1000][1000];
+
+    int n;
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+
+    for(int li=0;li<n;li++){
+        for(int lj=0;lj<n;lj++){     // extract all possible top left
+            
+            for(int bi=li;bi<n;bi++){
+                for(int bj=lj;bj<n;bj++){    // extract all posible bottom right
+
+                    int sum = 0;
+                    for(int i=li;i<=bi;i++){
+                        for(int j=lj;j<=bj;j++){    // sum of all elements of current submatrix
+                            sum += a[i][j];
+                        }
+                    }
+                    cout<<sum<<endl;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
