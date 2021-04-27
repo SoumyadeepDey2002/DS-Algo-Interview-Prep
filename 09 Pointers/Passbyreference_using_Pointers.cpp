bb5ecbee4f279475d2c960b25fb59474802980c9
Soumyadeep Dey <74682979+SoumyadeepDey2002@users.noreply.github.com>
2021-04-27 23:18:00 +0530

    Add files via upload

---
+++ 09 Pointers/Passbyreference_using_Pointers.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void increment(int* aptr){
+    *aptr = *aptr + 1;
+    cout<<"Inside Function : "<<*aptr<<endl;
+}
+
+int main()
+{
+    int a = 10;
+    increment(&a);
+
+    cout<<"Inside Main : "<<a;
+
+    return 0;
+}
