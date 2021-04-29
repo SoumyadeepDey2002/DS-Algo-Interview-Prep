9d8ae4bc10fe71a4553cc1ec55f671bad1f00535
Soumyadeep Dey <74682979+SoumyadeepDey2002@users.noreply.github.com>
2021-04-29 21:18:18 +0530

    Update FindFunction.cpp

--- 11 Algorithms_STL/FindFunction.cpp
+++ 11 Algorithms_STL/FindFunction.cpp
@@ -1,7 +1,7 @@
 //Search --> find
 // Takes in 3 parameters, find(start,end,key)
 //start - address to start searching from.
-//end - search ends before this address
+//end - search ends before this address ,arr+n ,what ever n is doesn't have to be length of arr
 //this function returns the address where it found the key, if it did not found key it will return the address arr+n
 
 #include<bits/stdc++.h>
