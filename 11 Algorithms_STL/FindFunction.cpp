eabca3e25f8211997837be9a56113e01e8f63448
Soumyadeep Dey <74682979+SoumyadeepDey2002@users.noreply.github.com>
2021-04-29 21:09:21 +0530

    Add files via upload

---
+++ 11 Algorithms_STL/FindFunction.cpp
@@ -0,0 +1,29 @@
+//Search --> find
+// Takes in 3 parameters, find(start,end,key)
+//start - address to start searching from.
+//end - search ends before this address
+//this function returns the address where it found the key, if it did not found key it will return the address arr+n
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1,10,11,9,100};
+    int n = sizeof(arr)/sizeof(int);
+
+    int key;
+    cin>>key;
+
+    auto ptr = find(arr,arr+n,key);
+    int index = ptr - arr;          // lets say base address is 100 and key was found in 102 ,102-100 = 2 ,2nd index
+
+    if(index == n){
+        cout<<key<<" is not present"<<endl;
+    }
+    else{
+        cout<<index;
+    }
+
+    return 0;
+}
