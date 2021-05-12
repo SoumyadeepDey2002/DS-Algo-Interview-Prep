5530d0f3a05279af2aef768e695476c5dc417dca
Soumyadeep Dey <74682979+SoumyadeepDey2002@users.noreply.github.com>
2021-05-12 16:53:13 +0530

    Add files via upload

---
+++ 13 Generic Programming/STL_Containers_intro.cpp
@@ -0,0 +1,30 @@
+/*
+
+Containers in STL
+
+Sequence Containers: implement data structures which can be accessed in a sequencial manner.
+* vector
+* list
+* deque
+* arrays
+* forward list
+
+Container Adaptors: provide different interface for sequential containers.
+* queue
+* priority queue
+* stack
+
+Associative Containers: implement sorted data structures that can be quickly searched
+O(logn) complexity.
+* set
+* multiset
+* map
+* multimap
+
+Unordered Associative Containers: implement data structures that can be quickly searched
+* unordered set
+* unordered multiset
+* unordered map
+* unordered multimap
+
+*/
